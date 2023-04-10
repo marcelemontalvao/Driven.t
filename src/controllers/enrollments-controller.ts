7c407289086284a21a2186da97e6f63d069a91ce
Marcele Montalvão <mmontalvaoblog@outlook.com>
2023-04-10 17:17:27 -0300

    fix: another bugs

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -34,7 +34,7 @@ export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response
     const address = await enrollmentsService.getAddressFromCEP(cep);
     res.status(httpStatus.OK).send(address);
   } catch (error) {
-    if (error.name === 'NotFoundError') {
+    if (error.name === 'NotFoundError' || 'invalid CEP') {
       return res.sendStatus(httpStatus.NO_CONTENT);
     }
   }
